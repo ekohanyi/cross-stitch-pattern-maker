20d5636dd30af65afc41dcb5aae0a8cf5eb6fbc6
emma kohanyi <emma.kohanyi@fieldtest.la>
2018-01-15 19:04:16 -0800

    created gridsquare and grid, added dynamic grid size input

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,20 +1,37 @@
-/*
-    ./client/components/App.jsx
-*/
 import React from 'react';
+import Grid from './grid.jsx';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			name: "pattern maker placeholder"
+			size: 10,
+			error: ''
+		}
+
+		this.inputChanged = this.inputChanged.bind(this);
+	}
+
+	inputChanged() {
+		if(parseInt(this.gridSizeInput.value) > 60) {
+			this.setState({ error: "Please enter an integer less than 60." });
+		}
+		else {
+			this.setState({ size: this.gridSizeInput.value, error: "" });
 		}
 	}
+
 	render() {
 		return (
 			<div style={{textAlign: 'center'}}>
-				<h1>{this.state.name}</h1>
+				<input 
+					onChange={() => this.inputChanged()} 
+					ref={input => { this.gridSizeInput = input }}
+					value={this.state.size}
+				/>
+				{this.state.error ? <div className="error">{this.state.error}</div> : ''}
+				<Grid size={this.state.size} />
 			</div>);
 	}
 }
---
+++ scripts/grid.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export default class Grid extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			gridSize: props.size
+		};
+
+		this.renderRow = this.renderRow.bind(this);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if(nextProps.size !== this.state.gridSize) {
+			this.setState({ gridSize: nextProps.size })
+		}
+	}
+
+	renderRow() {
+		let { gridSize } = this.state;
+		var gridRows = [];
+
+		for(var i = 0; i < gridSize; i++) {
+			var row = [];
+
+			for(var j = 0; j < gridSize; j++){
+				row.push(<GridSquare key={'row_' + i + '_cell_' + j}/>);
+			}
+			gridRows.push(<GridRow key={'row_' + i} children={row} />)
+		}
+
+		return gridRows;
+	}
+
+	render() {
+		var row = this.renderRow();
+		return (<div className="grid-container">{row}</div>);
+	}
+}
+
+function GridRow(props) {
+  return (<div className={'row cf'}>{props.children}</div>);
+}
+
+class GridSquare extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			color: '#fff'
+		}
+	}
+	render() {
+		return (
+			<div className="cell" style={{backgroundColor: this.state.color}}></div>);
+	}
+}
